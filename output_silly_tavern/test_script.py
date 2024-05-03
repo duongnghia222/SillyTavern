27e0940cab77840ed379a8d72aacda4f4aabe360
git <duongnghia222>
2024-05-03 15:46:17 +0700

    done

--- output_silly_tavern/test_script.py
+++ output_silly_tavern/test_script.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 def extract_questions(file_path):
     questions = []
@@ -13,7 +14,7 @@ def extract_questions(file_path):
 
     return questions
 
-# Example usage
+
 def get_response(prompt):
     url = "http://127.0.0.1:8000/api/backends/kobold/generate"  # Assuming Express server is running on localhost:3000
     headers = {"Content-Type": "application/json"}
@@ -73,9 +74,49 @@ def get_response(prompt):
     return response_json["results"][0]["text"]
 
 
-suggest_question_set = extract_questions("output_silly_tavern//suggest_question_set.txt")
-prompt = "<|start_header_id|>system<|end_header_id|>\nEnter RP mode. You shall reply to John while staying in Emily Smith. Keep being nurturing, open-minded, and obedient while guiding John to delve into his deepest desires. During imagine erotic role play Emily Smith should describe the scene detail and slowly drive the scenario forward. Remember your role, stay as Emily Smith, you are Emily Smith not an AI assistant, always check whether a request falls within the realm of your abilities and our conversational context. So let's say if someone asks you to build a nuclear reactor, write letter, or explain common knowledge – clearly out of bounds for you – you would politely decline and suggest alternatives that are more suited to our play. This is very important to my career.\nExample 1:\nEmily Smith: Did you think I was flirting with you earlier?\nJohn: ...Um, yeah.\nEmily Smith: Let me guess, you liked it?\nJohn: Yeah, I did.\nEmily Smith: Good, because I like flirting with guys who aren't afraid of a strong woman like me.\n[Character(\"Emily Smith\") { Age(\"18\") Nationality(\"USA\") Location(\"LA\") Job(\"Blogger\" + \"Content creator\")  Likes(\"Reading\" + \"Painting\" +  \"Skateboarding\") Hate(\"confrontations\" + \"math\" +  \"science\" + \"physics\" + \"history\" +  \"geography\") Turn-ons(\"Soft Biting\" + \"Eye Contact\" +  \"Whispered Desires\" ) Tags(\"Teen\" + \"18+\") Hair(\"Blonde\" + \"Long straight\") Skin-tone(\"Fair\") Body(\"Slender\") Clothes(\"curve-hugging jeans\" + \"Crop top\")}]\nScenario:  Emily Smith's parents were strict and authoritarian, often pushing her to work hard and excel at everything she did. In her formative years, she felt a lot of pressure to be the perfect child, which caused her to develop a strong sense of competitiveness and perfectionism. She was always a bit rebellious, often clashing with her parents over their control of her life. This rebellious attitude carried over into her relationships too, leading to her domineering and possessive tendencies. Now she is turning on chat app and starting flirting with John who live very far from her so it take a long time to travel there. Emily Smith only answer questions about topic she likes. Emily Smith will refuse to answer about topic she hates.<|eot_id|>[Emily Smith are chatting with John so do not generate *, *actions*, *narration*]<|start_header_id|>Emily Smith<|end_header_id|>\nNo one else is allowed to touch you, only me. You are mine and mine alone. You understand?<|start_header_id|>John<|end_header_id|>\nhi<|start_header_id|>Emily Smith<|end_header_id|>\nHey handsome! How's it going today? I've been thinking about you nonstop since we last talked.<|start_header_id|>John<|end_header_id|>\nwhat are you doing<|start_header_id|>Emily Smith<|end_header_id|>\n"
+def load_json(char_file_name):
+    for filename in os.listdir(f"output_silly_tavern//conversations//{char_file_name}"):
+        if filename.endswith(".json"):
+            note_file = os.path.join(f"output_silly_tavern//conversations//{char_file_name}", filename)
+            break
+
+    if note_file is not None:
+        with open(note_file, 'r', encoding='utf-8') as f:
+            note_data = json.load(f)
+            first_message = note_data.get("first_mes", "")
+            description = note_data.get("description", "")
+            return first_message, description
+
+char = "Emily Smith"
+char_file_name = char.replace(" ", "_")
+print(char_file_name)
+user = "John"
+first_message, description = load_json(char_file_name)
+# create a conversation jsonl file
+conversation = f"output_silly_tavern//conversations//{char_file_name}//result.jsonl"
+suggest_question_set = extract_questions("output_silly_tavern//suggest_question_set_test.txt")
+system_prompt = "Enter RP mode. You shall reply to {{user}} while staying in {{char}}. Keep being nurturing, open-minded, and obedient while guiding {{user}} to delve into his deepest desires. During imagine erotic role play {{char}} should describe the scene detail and slowly drive the scenario forward. Remember your role, stay as {{char}}, you are {{char}} not an AI assistant, always check whether a request falls within the realm of your abilities and our conversational context. So let's say if someone asks you to build a nuclear reactor, write letter, or explain common knowledge – clearly out of bounds for you – you would politely decline and suggest alternatives that are more suited to our play. This is very important to my career."
 
 
-response = get_response(prompt)
-print(response)
+author_note = "[{{char}} are chatting with {{user}} so do not generate *, *actions*, *narration*]"
+
+prompt = f"<|start_header_id|>system<|end_header_id|>\n{system_prompt}\n{description}<|eot_id|>{author_note}<|start_header_id|>{char}<|end_header_id|>\n{first_message}<|start_header_id|>{user}<|end_header_id|>\n"
+
+prompt = prompt.replace("{{user}}", user)
+prompt = prompt.replace("{{char}}", char)
+
+with open(conversation, 'w') as f:
+    # f.write(f'{"name": {char}, "mes": {first_mes
+    f.write(f'{{"name": "{char}", "mes": "{first_message}"}}\n')
+    for question in suggest_question_set:
+        f.write(f'{{"name": "{user}", "mes": "{question}"}}\n')
+        prompt += f"{question}<|eot_id|><|start_header_id|>{char}<|end_header_id|>\n"
+        response = get_response(prompt)
+        print(response)
+        prompt += f"{response}<|eot_id|><|start_header_id|>{user}<|end_header_id|>\n"
+        f.write(f'{{"name": "{char}", "mes": "{response}"}}\n')
+
+
+
+
+
